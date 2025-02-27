3e17c91d28e0ff3db0019ca83b50d93eef1f344b
lookandhqte <roselifemeow@gmail.com>
2025-02-27 18:06:07 +0300

    Looks better, but 19/20 passed tests yet

--- promise.js
+++ promise.js
@@ -1,7 +1,6 @@
 "use strict";
 const cache = {
   fn: [],
-  values: [],
   results: [],
 };
 
@@ -14,39 +13,21 @@ function memoize(fn) {
   return function (...args) {
     let fnIndex = cache.fn.indexOf(fn);
     let argsKey = JSON.stringify(args);
-    let valuesIndex = cache.values.indexOf(argsKey);
-    if (fnIndex !== -1 && valuesIndex !== -1) {
-      //нужно добавить проверку на undefined в ress
-      //бывают ситуации, когда функция кэширована
-      // значения кэшированы, но результат конкретно у этой функции и этих значений еще не вычислялся
-      //в таком случае .res еще не существует, и нужно создать объект новый с этой же функцией и values
-      // и вычислить результат
-      const ress = cache.results.find(
-        (elem) => elem.fnId == fnIndex && elem.valuesId == valuesIndex
-      );
-      if (!ress || ress == undefined) {
-        let resId = cache.results.length;
-        cache.results[resId] = {
-          index: resId,
-          fnId: fnIndex,
-          valuesId: valuesIndex,
-          res: fn(...args),
-        };
-        return cache.results[resId].res;
-      } else {
-        return ress.res;
-      }
-    }
-    if (cache.values.includes(argsKey) == false) {
-      cache.values.push(argsKey);
-      let resId = cache.results.length;
-      cache.results[resId] = {
-        index: resId,
-        fnId: cache.fn.indexOf(fn),
-        valuesId: cache.values.indexOf(argsKey),
+    const ress = cache.results.find(
+      (elem) => elem.fnId == fnIndex && elem.values == argsKey
+    );
+    if (!ress || ress == undefined) {
+      cache.results.push({
+        fnId: fnIndex,
+        values: argsKey,
         res: fn(...args),
-      };
-      return cache.results[resId].res;
+      });
+      const resulto = cache.results.find(
+        (elem) => elem.fnId == fnIndex && elem.values == argsKey
+      );
+      return resulto.res;
+    } else {
+      return ress.res;
     }
   };
 }
